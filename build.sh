b4b8650cabab395dd96e3c0d0606e07c3c21f62a
Jason Kingsbury <jason@relva.co.uk>
2019-09-28 18:10:53 +0100

    build script only selects most recent tag

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x
 
 PKG="github.com/relvacode/fifo"
 
-VERSION=`git tag`
-COMMIT=`git rev-parse HEAD`
+VERSION=`git tag | tail -n 1`
+COMMIT=`git rev-parse HEAD | tail -n 1`
 
-CGO_ENABLED=0 go build -ldflags "-X ${PKG}/build.Version=${VERSION} -X ${PKG}/build.Commit=${COMMIT}" $@ "${PKG}/cmd/fifo"
+CGO_ENABLED=0 go build -ldflags "-X ${PKG}/build.Version=${VERSION} -X ${PKG}/build.Commit=${COMMIT} -X ${PKG}/build.Build=docker" $@ "${PKG}/cmd/fifo"
